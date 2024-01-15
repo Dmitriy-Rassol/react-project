2278df5f6eacef1e11174e5184d693559f440ae6
Dmitriy-Rassol <devlion69@gmail.com>
2024-01-16 02:26:42 +0300

    Добавил условие работы таймера

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,25 +1,39 @@
 import { useState, useEffect } from 'react';
 
+
 const Timer = () => {
-  const [currentTime, setCurrentTime] = useState(new Date());
-
-  useEffect(() => {
-    const updateCurrentTime = () => {
-      setCurrentTime(new Date());
-    };
-
-    const intervalId = setInterval(updateCurrentTime, 1000);
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []); 
-
-  return (
-    <div style={{textAlign:"center"}}>
-      <h2 style={{color:"purple"}}>Текущее время: {currentTime.toLocaleTimeString()}</h2>
-    </div>
-  );
-};
-
-export default Timer;
+    const [currentTime, setCurrentTime] = useState(new Date());
+    const [expired, setExpired] = useState(false);
+  
+    useEffect(() => {
+      const updateCurrentTime = () => {
+        const newTime = new Date();
+        setCurrentTime(newTime);
+  
+        const expirationTime = new Date(currentTime.getTime() + 1 * 60 * 1000);
+        if (newTime > expirationTime) {
+          setExpired(true);
+          clearInterval(intervalId);
+        }
+      };
+  
+      const intervalId = setInterval(updateCurrentTime, 1000);
+  
+      return () => {
+        clearInterval(intervalId);
+      };
+    }, [currentTime]);
+  
+    return (
+      <div>
+        <p style={{textAlign:"center"}}>Текущее время: {currentTime.toLocaleTimeString()}</p>
+        {expired ? (
+          <p style={{ color: 'red' }}>Время истекло!</p>
+        ) : (
+          <p>Таймер будет остановлен через 1 минуту</p>
+        )}
+      </div>
+    );
+  };
+  
+  export default Timer;
