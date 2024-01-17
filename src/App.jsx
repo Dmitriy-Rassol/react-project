fb3ef4ee458a17ab5d87fe4243db53ad5bca1140
Dmitriy-Rassol <devlion69@gmail.com>
2024-01-17 04:59:15 +0300

    Кнопка меняет цвет

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import './App.css'
-import Button from './components/Button.jsx';
-import Input from './components/Input.jsx';
+import "./App.css";
+import Button from "./components/Button.jsx";
+import Input from "./components/Input.jsx";
 
-import { useState } from 'react';
-export default function App () {
-  const [inputValue, setInputValue] = useState('');
+import { useState } from "react";
+export default function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [clickCount, setClickCount] = useState(1);
 
   const handleClick = () => {
-    setInputValue('');
+    setInputValue("");
+    setClickCount(clickCount + 1);
   };
 
   const handleChange = (event) => {
     setInputValue(event.target.value);
   };
 
+  const buttonClassName = clickCount % 2 === 0 ? 'red' : '';
+
   return (
-    <div className='App'>
+    <div className="App">
       <Input value={inputValue} onChange={handleChange} />
-      <Button onClick={handleClick} text="Очистить поле"/>
+      <button className={buttonClassName} onClick={handleClick}>
+        Очистить поле
+      </button>
     </div>
   );
 }
-
-
